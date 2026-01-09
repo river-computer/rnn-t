7eb593fde7f42a927f892da4335058444d5c8e52
Oseh Mathias <o@matmail.me>
2026-01-09 11:39:38 +0800

    Export SimplifiedRNNTLoss

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,11 @@
-from .losses import CTCLoss, RNNTLoss
+from .losses import CTCLoss, RNNTLoss, SimplifiedRNNTLoss
 from .metrics import compute_per, compute_wer, MetricTracker
 from .s3 import S3Checkpoint, upload_checkpoint, download_checkpoint, checkpoint_exists, list_checkpoints
 
 __all__ = [
     "CTCLoss",
     "RNNTLoss",
+    "SimplifiedRNNTLoss",
     "compute_per",
     "compute_wer",
     "MetricTracker",
